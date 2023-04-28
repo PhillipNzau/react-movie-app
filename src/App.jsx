5e672e640fb992b6a3dcf7c166aac7dee4783125
phillipnzau <nzauonline78@gmail.com>
2023-04-28 16:20:40 +0300

    fixed url

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,7 @@ import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 import "./App.css";
 
-const API_URL = "http://www.omdbapi.com?apiKey=f1c010d";
-
-const movie1 = {
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-  Title: "Batman v Superman: Dawn of Justice",
-  Type: "movie",
-  Year: "2016",
-  imdbID: "tt2975590",
-};
+const API_URL = "https://www.omdbapi.com?apiKey=f1c010d";
 
 function App() {
   const [movies, setMovies] = useState([]);

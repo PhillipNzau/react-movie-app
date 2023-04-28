36c62cf4f4ab531c708a32af113b4ec742854ef4
phillipnzau <nzauonline78@gmail.com>
2023-04-28 16:10:47 +0300

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
+import "./App.css";
+
+const API_URL = "http://www.omdbapi.com?apiKey=f1c010d";
+
+const movie1 = {
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+  Title: "Batman v Superman: Dawn of Justice",
+  Type: "movie",
+  Year: "2016",
+  imdbID: "tt2975590",
+};
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
+
+    setMovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("superman");
+  }, []);
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          type="text"
+          placeholder="search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
